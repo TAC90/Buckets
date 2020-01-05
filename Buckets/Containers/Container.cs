5527339c35bb6aeb8e08ea84ed72ac9b9c0251f1
TAC90 <troycramer90@gmail.com>
2020-01-05 19:49:09 +0100

    Transfer fix

--- Buckets/Containers/Container.cs
+++ Buckets/Containers/Container.cs
@@ -6,6 +6,7 @@ namespace Buckets.Containers
     abstract class Container
     {
         #region Properties
+        static public bool AllowOverflow = false;
         //Decision: Place properties inside base class or derived classes?
         private double minSize; //Technically only used for bucket. Move to bucket and override the Size property?
         private double size;
@@ -63,10 +64,8 @@ namespace Buckets.Containers
         }
         public bool Empty(double amount)
         {
-            if (amount > Content)
+            if (amount > Content) //Check if amount to remove exists
             {
-                //Emptied too much
-                Content = 0;
                 return false;
             }
             else
@@ -77,29 +76,78 @@ namespace Buckets.Containers
         }
         #endregion
 
-        public void Fill(double amount)
+        public bool Fill(double addedAmount)
         {
-            double total = amount + Content; //Calculate total of 
+            double total = addedAmount + Content; //Calculate total of 
             if (total > Size) //Check overflow
             {
-                Console.WriteLine($"{Name} has overflowed {total - Size}.");
-                Content = Size;
+                if (AllowOverflow)
+                {
+                    Console.WriteLine($"{this.Name}({this.Content}/{this.Size}) has overflowed {total - Size}.");
+                    Content = Size;
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"{this.Name}({this.Content}/{this.Size}) would overflow, action aborted.");
+                    return false;
+                }
             }
             else
             {
                 Content = total;
-            }
-        }
-        public bool Transfer(Container source, Container target, int amount = null) //Transfer from source to target with possible value
-        {
-            if(amount == null) { amount = source.Content; } //Check if amount was given, otherwise take amount in source
-            if (amount <= source.Content) //Check no overflow
-            {
-                target.Fill(amount);
-                source.Empty(amount);
+                Console.WriteLine($"Added {addedAmount} to {Name}({Content}/{Size}).");
                 return true;
             }
-            else { return false; } //Overflow
+        }
+        public Transfer(Container target, int movedAmount = null) //Transfer amount to target
+        {
+            if (movedAmount == null) { movedAmount = this.Content; } //Check if amount was given, otherwise take amount in source
+            //Method that checks everything in advance, not using existing methods, doubling code
+            if (movedAmount > this.Content) //Check if source has enough
+            {
+                if (target.Content + movedAmount < target.Size) //Check if no overflow
+                {
+                    target.Content += movedAmount;
+                    this.Content -= movedAmount;
+                    Console.WriteLine($"Moved {movedAmount} from {this.Name}({this.Content}/{this.Size}) to {target.Name}({target.Content}/{target.Size})."); //Is there a way to be sure doubles get rounded properly when used as string?
+                }
+                else //It will overflow
+                {
+                    double overflow = target.Content + movedAmount - target.Size; //Get overflow amount
+                    target.Content = target.size;
+                    if (AllowOverflow) //Lose the overflow
+                    {
+                        this.Content -= movedAmount;
+                        Console.WriteLine($"Moved {movedAmount} from {this.Name}({this.Content}/{this.Size}) to {target.Name}({target.Content}/{target.Size}) and overflowed with {overflow}.");
+                    }
+                    else //Return the overflow to the source
+                    {
+                        this.Content = overflow;
+                        Console.WriteLine($"Moved {movedAmount - overflow} from {this.Name}({this.Content}/{this.Size}) to {target.Name}({target.Content}/{target.Size})."); 
+                    }
+                }
+            }
+            else { Console.WriteLine($"Transfer cancelled, not enough ({movedAmount}) in {this.Name}({this.Content}/{this.Size})."); }
+
+
+            //Method that uses other methods, and undoes method if later methods fail. 
+            //OUTDATED, does not include all possibilities
+            /*if (this.Empty(amount))
+            {
+                if (target.Fill(this.content))
+                {
+                    Console.WriteLine($"Moved {amount} from {this.Name}({this.Content}/{this.Size}) to {target.Name}({target.Content}/{target.Size}).");
+                }
+                else
+                {
+                    this.Fill(amount);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Transfer Failed, {this.Name}({this.Content}/{this.Size}) does not have {amount}");
+            }*/
         }
     }
 
